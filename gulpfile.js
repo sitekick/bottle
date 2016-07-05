8f1d3306600b610fb87beebf26798a47845eacce
Hunter Williams <hunter@sitekick.com>
2016-07-05 16:10:09 -0400

    tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
 	
 var jsSources = [
@@ -15,9 +16,24 @@ var jsSources = [
 	'components/scripts/chart.init.js'
 	];
 	
+var sassSources = ['components/sass/style.scss'];
+	
 gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
+
+gulp.task('compass', function() {
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/img',
+			style: 'expanded'
+		}))
+		.on('error',gutil.log)
+		.pipe(gulp.dest('builds/development/css'))
+});
+
+gulp.task('default', ['js','compass']);
