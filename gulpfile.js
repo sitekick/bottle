547d75bac5b94ba8fc7b8cf0892a3fc802500411
Hunter Williams <hunter@sitekick.com>
2016-07-05 15:30:12 -0400

    concat js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
-
-gulp.task('log', function() {
-	gutil.log('bottle log');
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat');
+	
+var jsSources = [
+	'components/scripts/data/products.js',
+	'components/scripts/data/search.js',
+	'components/scripts/searchfield.js',
+	'components/scripts/modernizr/modernizr.3.3.1.mq.addcssclass.min.js',
+	'components/scripts/menus.js',
+	'components/scripts/jquery.autocomplete.min.js',
+	'components/scripts/jquery.autocomplete.init.js',
+	'components/scripts/chart.min.js',
+	'components/scripts/chart.init.js'
+	];
+	
+gulp.task('js', function() {
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
 });
